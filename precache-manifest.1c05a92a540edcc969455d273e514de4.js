57a066b872d761c373faaa1f05a5ba5c3710003f
DirkViljoen <dirkcharl.viljoen@gmail.com>
2019-08-08 15:08:15 +0200

    Updates

---
+++ precache-manifest.1c05a92a540edcc969455d273e514de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a28b532586bb797756c7a56e8e38434",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b9eb7dd1a4a9c8d57d23",
+    "url": "/monsters-rolodex/static/css/main.bd4bd04e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b9eb7dd1a4a9c8d57d23",
+    "url": "/monsters-rolodex/static/js/main.2674ce3c.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
